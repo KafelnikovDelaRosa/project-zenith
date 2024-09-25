98d4113ef2ecb70662581e6335d0847e2db0e167
KafelnikovDelaRosa <145114297+KafelnikovDelaRosa@users.noreply.github.com>
2024-09-25 12:20:15 +0800

    added socials section on index page

---
+++ resources/views/components/cloud.blade.php
@@ -0,0 +1,29 @@
+<div class="flex flex-col items-center w-full h-full lg:h-[20rem] px-12">
+    <h1 class="font-extrabold text-4xl">Our Socials</h1>
+    <div class="lg:flex gap-12">
+        <div class="flex flex-col items-center gap-3 px-8 py-10 rounded-3xl ">
+        <span>
+            <svg class="text-indigo-700 opacity-70" width="38" height="38" viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4l11.733 16h4.267l-11.733 -16z" /><path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" /></svg>
+        </span>
+        <p class="text-2xl font-extrabold text-dark-grey-900">Twitter</p>
+        <p class="text-base leading-7 text-dark-grey-600 text-center w-[15rem]">Reach out to us by X</p>
+        <a class="text-lg font-bold text-purple-blue-500" href="tel:+516-486-5135">+516-486-5135</a>
+        </div>
+        <div class="flex flex-col items-center gap-3 px-8 py-10 rounded-3xl ">
+            <span>
+                <svg class="text-indigo-700 opacity-70"  width="38" height="38" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <rect x="3" y="5" width="18" height="14" rx="2" />  <polyline points="3 7 12 13 21 7" /></svg>
+            </span>
+            <p class="text-2xl font-extrabold text-dark-grey-900">Email</p>
+            <p class="text-base leading-7 text-dark-grey-600 text-center w-[15rem]">Reach out to us by sending email</p>
+            <a class="text-lg font-bold text-purple-blue-500" href="tel:+516-486-5135">+516-486-5135</a>
+        </div>
+        <div class="flex flex-col items-center gap-3 px-8 py-10 rounded-3xl ">
+            <span>
+                <svg class="text-indigo-700 opacity-70" width="38" height="38"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" /></svg>
+            </span>
+            <p class="text-2xl font-extrabold text-dark-grey-900">Facebook</p>
+            <p class="text-base leading-7 text-dark-grey-600 text-center w-[15rem]">Reach out to us by clicking this link</p>
+            <a class="text-lg font-bold text-purple-blue-500" href="tel:+516-486-5135">+516-486-5135</a>
+        </div>
+    </div>
+</div>
--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -2,4 +2,5 @@
 @section('content')
     <x-banner />
     <x-carousel />
+    <x-cloud />
 @endsection
